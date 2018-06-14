0d6b3626d9f80883e8f2a63e3e2dc1fd209c177d
Carol <ckamau.gaceri@gmail.com>
2018-06-14 15:13:51 +0300

    migration: weapons + model + controller

---
+++ LindaSilaha/database/migrations/2018_06_14_115025_create_weapons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeaponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('serialNo');
+            $table->integer('category');
+            $table->integer('type');
+            $table->integer('mobility');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weapons');
+    }
+}
